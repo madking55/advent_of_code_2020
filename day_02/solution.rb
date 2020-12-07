3d734901d5449b7824b4344fa618675b78d10c89
madking55 <madking55@gmail.com>
2020-12-07 21:44:55 +0100

    Finish day_02

--- day_02/solution.rb
+++ day_02/solution.rb
@@ -1,9 +1,11 @@
 example_data = File.readlines("example.txt")
 input_data = File.readlines("input.txt")
 
-def solve(input)
-  valid_lines = 0
+# Part One
+
+def solve_with_range(input)
   lines = input.map { |line| line_to_hash(line)}
+  valid_lines = 0
   lines.each do |line_hash|
     count = char_count(line_hash[:char], line_hash[:password])
     valid_lines += 1 if (line_hash[:min]..line_hash[:max]).include?(count)
@@ -24,5 +26,28 @@ def char_count(target, string)
   count
 end
 
-p solve(example_data)
-p solve(input_data)
+p solve_with_range(example_data)
+p solve_with_range(input_data)
+
+# Part Two // 0(n)
+
+def solve_with_positions(input)
+  lines = input.map { |line| line_to_hash_positions(line)}
+  valid_lines = 0
+  lines.each do |line_hash|
+    is_at_pos_1 = line_hash[:password][line_hash[:pos_1]] == line_hash[:char]
+    is_at_pos_2 = line_hash[:password][line_hash[:pos_2]] == line_hash[:char]
+    valid_lines += 1 if ((is_at_pos_1 || is_at_pos_2) && !(is_at_pos_1 && is_at_pos_2))
+  end
+  valid_lines
+end
+
+def line_to_hash_positions(line)
+  range_segment, char_segment, password = line.split(' ')
+  pos_1, pos_2 = range_segment.split('-').map(&:to_i)
+  char = char_segment[0]
+  line_hash = {pos_1: pos_1 - 1, pos_2: pos_2 - 1, char: char, password: password}
+end
+
+p solve_with_positions(example_data)
+p solve_with_positions(input_data)
