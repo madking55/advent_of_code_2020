0b8ba878edff10a654e8e4f172654400e0e288ea
madking55 <madking55@gmail.com>
2020-12-10 13:09:29 +0100

    Finish day_05

--- day_05/solution.rb
+++ day_05/solution.rb
@@ -1,15 +1,19 @@
 example_data = File.readlines('example.txt').map(&:chomp)
 input_data = File.readlines('input.txt').map(&:chomp)
 
+# Part One
+
 def seats_ids(input)
-  input.map do |passport|
-    bits = {'F' => '0', 'B' => '1', 'L' => '0', 'R' => '1'}
-    passport.gsub!(/\w/, bits)
-    row_as_binary, column_as_binary = [passport[0..6], passport[7..-1]]
-    row = row_as_binary.to_i(2)
-    column = column_as_binary.to_i(2)
-    row * 8 + column
-  end
+  input.map { |seat| get_seat_id(seat) }
+end
+
+def get_seat_id(seat)
+  bits = {'F' => '0', 'B' => '1', 'L' => '0', 'R' => '1'}
+  seat_in_binary = seat.gsub(/\w/, bits)
+  row_in_binary, column_in_binary = [seat_in_binary[0..6], seat_in_binary[7..-1]]
+  row = row_in_binary.to_i(2)
+  column = column_in_binary.to_i(2)
+  row * 8 + column
 end
 
 def find_max_id(input)
@@ -18,4 +22,18 @@ end
 
 p find_max_id(example_data)
 p find_max_id(input_data)
+
+# Part Two
+
+def find_my_seat_id(input)
+  seats_ids(input)
+  sorted_ids = seats_ids(input).sort
+  for i in (1...sorted_ids.length) do 
+    current_seat = sorted_ids[i]
+    next_seat = sorted_ids[i + 1]
+    return current_seat + 1 if current_seat + 1 != next_seat
+  end
+end
+
+p find_my_seat_id(input_data)
 
